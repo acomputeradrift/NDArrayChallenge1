27ba1183cc56a8d7fa526a2ad2686d8ba385a24f
Jamie Feeny <feeny.jamie@gmail.com>
2018-07-07 13:58:00 -0700

    working grocery list with comments

--- NDArrayChallenge1/main.m
+++ NDArrayChallenge1/main.m
@@ -5,13 +5,24 @@
 //  Created by Jamie on 2018-07-07.
 //  Copyright © 2018 Jamie. All rights reserved.
 //
-
 #import <Foundation/Foundation.h>
-
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[])
+{
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+  
+
+    // Create an mutable array wtih objects
+        NSMutableArray *groceryList = [[NSMutableArray alloc]initWithObjects:@"loaf of bread", @"container of milk", @"stick of butter", nil];
+        
+    // Iterate over the array
+        NSLog (@"Grocery List\n");
+        for (NSString *d in groceryList) {
+            NSLog(@"%@\n", d);
+        }
+        
+    
+        
     }
     return 0;
 }
